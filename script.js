8d55be71e82a254ebb490560a622f0d76772fb9e
Shivansh Raghav <shivanshraghav7@gmai.com>
2023-07-20 16:37:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-//your code here
+let drag=null
+let c= document.getElementsByClassName('image')
+for(let i=0;i<c.length;i++){
+    c[i].addEventListener('dragstart',(event)=>{
+        drag=event.target
+    })
+}
+let parent= document.getElementById("parent")
+parent.addEventListener('dragover',(event)=>{
+    event.preventDefault()
+})
+parent.addEventListener("drop",(event)=>{
+    event.preventDefault()
+    console.log(drag)
+    console.log(event)
+    let temp=drag.id
+    drag.id=event.target.id
+    event.target.id=temp
+})
